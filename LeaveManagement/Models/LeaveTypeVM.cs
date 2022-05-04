4917f516642c2a3a0516f47df3fd07152910d476
Choiz0 <sarahchoi.webdeveloper@outlook.com>
2022-05-05 02:20:44 +1000

    adding mapper and CRUD at leavetype model

---
+++ LeaveManagement/Models/LeaveTypeVM.cs
@@ -0,0 +1,17 @@
+﻿using LeaveManagement.Data;
+using System.ComponentModel.DataAnnotations;
+
+namespace LeaveManagement.Models
+{
+    public class LeaveTypeVM 
+    {
+        public int Id { get; set; }
+        [Display(Name = "Leave Type")]
+        [Required]
+        public string Name { get; set; }
+        [Display(Name ="Default Number Of Days")]
+        [Range(1,25, ErrorMessage ="Please Enter A Valid Number")]
+        [Required]
+        public int DefaultDays { get; set; }
+    }
+}
