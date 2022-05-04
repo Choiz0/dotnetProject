c687ddd29671a236e6bce874038e9d313e5a78c1
Choiz0 <sarahchoi.webdeveloper@outlook.com>
2022-05-04 17:57:46 +1000

    1st commit model created

---
+++ LeaveManagement/Data/Migrations/20220504065544_namechange.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LeaveManagement.Data.Migrations
+{
+    public partial class namechange : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "id",
+                table: "LeaveTypes",
+                newName: "Id");
+
+            migrationBuilder.RenameColumn(
+                name: "id",
+                table: "LeaveAllocations",
+                newName: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "LeaveTypes",
+                newName: "id");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "LeaveAllocations",
+                newName: "id");
+        }
+    }
+}
